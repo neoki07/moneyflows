097ce04d55b89cb2a983f0510015cd1cc9ec1de3
neoki <ot07neoki@gmail.com>
2024-12-21 18:52:12 +0900

    feat: setup shadcn/ui

--- src/app/tags/page.tsx
+++ src/app/tags/page.tsx
@@ -1,8 +1,15 @@
+import { Button } from "@/components/ui/button";
+import { PlusIcon } from "lucide-react";
+
 export default function Page() {
   return (
     <div className="px-6 py-8 gap-8 grid grid-rows-[2.25rem,1fr]">
       <div className="flex items-center">
-        <h1 className="font-bold text-2xl">タグ</h1>
+        <h1 className="font-bold text-2xl flex-1">タグ</h1>
+        <Button>
+          <PlusIcon />
+          タグを追加
+        </Button>
       </div>
       <div>This is the tags page.</div>
     </div>
