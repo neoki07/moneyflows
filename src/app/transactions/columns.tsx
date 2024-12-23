60230cf6848f1008af2ce9ccdb6204d0fcfa5412
neoki <ot07neoki@gmail.com>
2024-12-23 19:35:29 +0900

    feat: add transaction table

---
+++ src/app/transactions/columns.tsx
@@ -0,0 +1,58 @@
+"use client";
+
+import { Checkbox } from "@/components/ui/checkbox";
+import { ColumnDef } from "@tanstack/react-table";
+
+export type Transaction = {
+  id: string;
+  amount: number;
+  description: string;
+  date: string;
+  category: string;
+  tag: string;
+};
+
+export const columns: ColumnDef<Transaction>[] = [
+  {
+    id: "select",
+    header: ({ table }) => (
+      <Checkbox
+        checked={
+          table.getIsAllPageRowsSelected() ||
+          (table.getIsSomePageRowsSelected() && "indeterminate")
+        }
+        onCheckedChange={(value) => table.toggleAllPageRowsSelected(!!value)}
+        aria-label="Select all"
+      />
+    ),
+    cell: ({ row }) => (
+      <Checkbox
+        checked={row.getIsSelected()}
+        onCheckedChange={(value) => row.toggleSelected(!!value)}
+        aria-label="Select row"
+      />
+    ),
+    enableSorting: false,
+    enableHiding: false,
+  },
+  {
+    accessorKey: "date",
+    header: "日付",
+  },
+  {
+    accessorKey: "description",
+    header: "内容",
+  },
+  {
+    accessorKey: "amount",
+    header: "金額",
+  },
+  {
+    accessorKey: "category",
+    header: "カテゴリー",
+  },
+  {
+    accessorKey: "tag",
+    header: "タグ",
+  },
+];
--- src/app/transactions/page.tsx
+++ src/app/transactions/page.tsx
@@ -1,10 +1,69 @@
-export default function Page() {
+import { Transaction, columns } from "./columns";
+import { DataTable } from "./data-table";
+
+async function getData(): Promise<Transaction[]> {
+  return [
+    {
+      id: "728ed52a",
+      amount: 100,
+      description: "pending",
+      date: "2022/01/01 (土)",
+      category: "給料",
+      tag: "給料",
+    },
+    {
+      id: "728ed52b",
+      amount: 100,
+      description: "pending",
+      date: "2022/01/01 (土)",
+      category: "給料",
+      tag: "給料",
+    },
+    {
+      id: "728ed52c",
+      amount: 100,
+      description: "pending",
+      date: "2022/01/01 (土)",
+      category: "給料",
+      tag: "給料",
+    },
+    {
+      id: "728ed52d",
+      amount: 100,
+      description: "pending",
+      date: "2022/01/01 (土)",
+      category: "給料",
+      tag: "給料",
+    },
+    {
+      id: "728ed52e",
+      amount: 100,
+      description: "pending",
+      date: "2022/01/01 (土)",
+      category: "給料",
+      tag: "給料",
+    },
+    {
+      id: "728ed52f",
+      amount: 100,
+      description: "pending",
+      date: "2022/01/01 (土)",
+      category: "給料",
+      tag: "給料",
+    },
+  ];
+}
+
+export default async function Page() {
+  const data = await getData();
   return (
     <div className="px-6 py-8 gap-8 grid grid-rows-[2.25rem,1fr]">
       <div className="flex items-center">
         <h1 className="font-bold text-2xl">収支</h1>
       </div>
-      <div>This is the transactions page.</div>
+      <div>
+        <DataTable columns={columns} data={data} />
+      </div>
     </div>
   );
 }
