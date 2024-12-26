6a41f4d47fc975d5bb2058ee7891960567184a94
neoki <ot07neoki@gmail.com>
2024-12-26 21:09:38 +0900

    fix: update checkbox color

--- src/components/ui/checkbox.tsx
+++ src/components/ui/checkbox.tsx
@@ -13,7 +13,7 @@ const Checkbox = React.forwardRef<
   <CheckboxPrimitive.Root
     ref={ref}
     className={cn(
-      "peer size-5 grid place-content-center shrink-0 bg-white rounded-md border border-muted-foreground/50 ring-offset-background focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 data-[state=checked]:border-primary data-[state=checked]:bg-primary data-[state=checked]:text-primary-foreground",
+      "peer size-5 grid place-content-center shrink-0 bg-white rounded-md border border-muted-foreground/50 ring-offset-background focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 data-[state=checked]:border-[#007DFC] data-[state=checked]:bg-[#007DFC] data-[state=checked]:text-primary-foreground",
       className,
     )}
     {...props}
