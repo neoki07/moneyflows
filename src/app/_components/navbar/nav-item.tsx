3c2048dc65cbffbc7115dd68bde977526820a045
neoki <ot07neoki@gmail.com>
2024-12-29 13:15:00 +0900

    fix: update navbar styles

--- src/app/_components/navbar/nav-item.tsx
+++ src/app/_components/navbar/nav-item.tsx
@@ -10,11 +10,11 @@ type NavItemProps = Readonly<{
 export function NavItem({ link, icon: Icon, children }: NavItemProps) {
   return (
     <Link
-      className="hover:bg-slate-100 min-h-10 flex items-center px-3 py-1.5 rounded-lg text-slate-700"
+      className="hover:bg-slate-100 min-h-9 flex items-center px-3 py-1.5 rounded-lg text-slate-700"
       href={link}
     >
-      {Icon && <span className="mr-2">{<Icon className="size-5" />}</span>}
-      <span className="text-base font-semibold">{children}</span>
+      {Icon && <span className="mr-1.5">{<Icon className="size-5" />}</span>}
+      <span className="text-sm font-semibold">{children}</span>
     </Link>
   );
 }
--- src/app/_components/navbar/navbar.tsx
+++ src/app/_components/navbar/navbar.tsx
@@ -49,7 +49,7 @@ export function Navbar() {
           </li>
         </ul>
         <ul>
-          <div className="text-slate-500 text-sm font-semibold px-3 h-7 grid grid-cols-[1fr,1rem]">
+          <div className="text-slate-500 text-xs font-semibold px-3 h-7 grid grid-cols-[1fr,1rem]">
             ダッシュボード
             <IconPlus size={16} />
           </div>
