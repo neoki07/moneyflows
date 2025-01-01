faf5970495f976d0b24c0f4b6bd64d4b4259e644
neoki <ot07neoki@gmail.com>
2025-01-01 20:37:02 +0900

    feat: implement monthly-balance-card component

--- src/app/(home)/_components/monthly-balance-card.tsx
+++ src/app/(home)/_components/monthly-balance-card.tsx
@@ -1,3 +1,12 @@
 export function MonthlyBalanceCard() {
-  return <div className="rounded-lg border p-4">monthly balance card</div>;
+  return (
+    <div className="rounded-lg border p-4 flex flex-col gap-4">
+      <h2 className="text-[13px] leading-none font-semibold text-slate-700">
+        今月の収支差額
+      </h2>
+      <div className="font-semibold leading-none">
+        <span className="text-2xl leading-none me-1">123,456</span>円
+      </div>
+    </div>
+  );
 }
