1a72f2e41bf2d99d20a03395a0ee2af2a400b678
neoki <ot07neoki@gmail.com>
2025-01-01 20:34:11 +0900

    feat: implement total-balance-card component

--- src/app/(home)/_components/total-balance-card.tsx
+++ src/app/(home)/_components/total-balance-card.tsx
@@ -1,5 +1,12 @@
 export function TotalBalanceCard() {
   return (
-    <div className="rounded-lg border p-4">This is total balance card</div>
+    <div className="rounded-lg border p-4 flex flex-col gap-4">
+      <h2 className="text-[13px] leading-none font-semibold text-slate-700">
+        総資産
+      </h2>
+      <div className="font-semibold leading-none">
+        <span className="text-2xl leading-none me-1">123,456</span>円
+      </div>
+    </div>
   );
 }
