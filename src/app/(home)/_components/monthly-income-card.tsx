303490f8c76dd96671cc664a7bcbae6a399816e8
neoki <ot07neoki@gmail.com>
2025-01-01 20:35:08 +0900

    feat: implement monthly-income-card component

--- src/app/(home)/_components/monthly-income-card.tsx
+++ src/app/(home)/_components/monthly-income-card.tsx
@@ -1,5 +1,12 @@
 export function MonthlyIncomeCard() {
   return (
-    <div className="rounded-lg border p-4">This is monthly income card</div>
+    <div className="rounded-lg border p-4 flex flex-col gap-4">
+      <h2 className="text-[13px] leading-none font-semibold text-slate-700">
+        今月の収入
+      </h2>
+      <div className="font-semibold leading-none">
+        <span className="text-2xl leading-none me-1">123,456</span>円
+      </div>
+    </div>
   );
 }
