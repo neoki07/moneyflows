b97f73f85315f9015cffde81736be33367a8ed8a
neoki <ot07neoki@gmail.com>
2025-01-03 21:34:39 +0900

    feat: implement latest-transactions-card component

--- src/app/(home)/_components/latest-transactions-card.tsx
+++ src/app/(home)/_components/latest-transactions-card.tsx
@@ -1,7 +1,41 @@
+import { Badge } from "@/components/ui/badge";
+import { Separator } from "@/components/ui/separator";
+
 export function LatestTransactionsCard() {
   return (
-    <div className="rounded-lg border p-4">
-      This is latest transactions card
+    <div className="border rounded-lg p-6 col-span-4 space-y-6">
+      <h2 className="font-bold text-xl">最近の収支</h2>
+      <div className="space-y-4">
+        <ul className="space-y-2">
+          <span className="text-sm text-slate-700">11月23日 (土)</span>
+          <li className="grid grid-cols-[15rem,1fr,6rem] text-sm">
+            <span>給料</span>
+            <div className="flex flex-wrap gap-1">
+              <Badge variant="outline">給料</Badge>
+            </div>
+            <span className="text-right text-emerald-600">123円</span>
+          </li>
+          <li className="grid grid-cols-[15rem,1fr,6rem] text-sm">
+            <span>ローソン</span>
+            <div className="flex flex-wrap gap-1">
+              <Badge variant="outline">食費</Badge>
+              <Badge variant="outline">生活費</Badge>
+            </div>
+            <span className="text-right text-rose-600">-123円</span>
+          </li>
+        </ul>
+        <Separator />
+        <ul className="space-y-2">
+          <span className="text-sm text-slate-700">11月22日 (金)</span>
+          <li className="grid grid-cols-[15rem,1fr,6rem] text-sm">
+            <span>給料</span>
+            <div className="flex flex-wrap gap-1">
+              <Badge variant="outline">給料</Badge>
+            </div>
+            <span className="text-right text-emerald-600">123円</span>
+          </li>
+        </ul>
+      </div>
     </div>
   );
 }
