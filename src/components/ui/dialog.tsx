e8e2391373377949ab7f1862d4cc90a00bf01c55
neoki <ot07neoki@gmail.com>
2025-01-30 22:34:17 +0900

    fix: adjust dialog header spacing for improved visual layout

--- src/components/ui/dialog.tsx
+++ src/components/ui/dialog.tsx
@@ -59,7 +59,7 @@ const DialogHeader = ({
   className,
   ...props
 }: React.HTMLAttributes<HTMLDivElement>) => (
-  <div className={cn("flex flex-col space-y-1.5", className)} {...props} />
+  <div className={cn("flex flex-col space-y-2", className)} {...props} />
 );
 DialogHeader.displayName = "DialogHeader";
 
