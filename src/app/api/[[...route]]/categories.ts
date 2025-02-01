2c095bf816084eea71556c4708d2f73d1f6d8bd3
neoki <ot07neoki@gmail.com>
2025-02-02 00:52:12 +0900

    feat: add categories api endpoint

---
+++ src/app/api/[[...route]]/categories.ts
@@ -0,0 +1,35 @@
+import { getAuth } from "@hono/clerk-auth";
+import { zValidator } from "@hono/zod-validator";
+import { eq } from "drizzle-orm";
+import { Hono } from "hono";
+import { z } from "zod";
+
+import { db } from "@/db";
+import { categoryTable } from "@/db/schema";
+export const categories = new Hono().get(
+  "/",
+  zValidator(
+    "query",
+    z.object({
+      type: z.enum(["income", "expense"]).optional(),
+    }),
+  ),
+  async (c) => {
+    const auth = getAuth(c);
+
+    if (!auth?.userId) {
+      return c.json({
+        message: "You are not logged in.",
+      });
+    }
+
+    const { type } = c.req.valid("query");
+
+    const categories = await db
+      .select()
+      .from(categoryTable)
+      .where(type ? eq(categoryTable.type, type) : undefined);
+
+    return c.json({ categories });
+  },
+);
--- src/app/api/[[...route]]/route.ts
+++ src/app/api/[[...route]]/route.ts
@@ -1,25 +1,14 @@
-import { clerkMiddleware, getAuth } from "@hono/clerk-auth";
+import { clerkMiddleware } from "@hono/clerk-auth";
 import { Hono } from "hono";
 import { handle } from "hono/vercel";
+
+import { categories } from "./categories";
 
 const app = new Hono().basePath("/api");
 
 app.use("*", clerkMiddleware());
 
-export const apiRoutes = app.get("/", (c) => {
-  const auth = getAuth(c);
-
-  if (!auth?.userId) {
-    return c.json({
-      message: "You are not logged in.",
-    });
-  }
-
-  return c.json({
-    message: "You are logged in!",
-    userId: auth.userId,
-  });
-});
+export const apiRoutes = app.route("/categories", categories);
 
 export const GET = handle(apiRoutes);
 export const POST = handle(apiRoutes);
