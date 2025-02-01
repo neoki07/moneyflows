6217ea208bc7122271abac7f84f5ec3bba972b7b
neoki <ot07neoki@gmail.com>
2025-02-02 02:16:01 +0900

    feat: add post endpoint for creating categories

--- src/app/api/[[...route]]/categories.ts
+++ src/app/api/[[...route]]/categories.ts
@@ -1,24 +1,31 @@
 import { getAuth } from "@hono/clerk-auth";
 import { zValidator } from "@hono/zod-validator";
+import { createId } from "@paralleldrive/cuid2";
 import { and, eq } from "drizzle-orm";
 import { Hono } from "hono";
 import { z } from "zod";
 
 import { db } from "@/db";
 import { categoryTable } from "@/db/schema";
-export const categories = new Hono().get(
-  "/",
-  zValidator(
-    "query",
-    z.object({
-      type: z.enum(["income", "expense"]).optional(),
-    }),
-  ),
-  async (c) => {
+
+const getCategoriesSchema = z.object({
+  type: z.enum(["income", "expense"]).optional(),
+});
+
+const createCategorySchema = z.object({
+  name: z
+    .string()
+    .min(1, "カテゴリー名を入力してください")
+    .max(20, "カテゴリー名は20文字以内で入力してください"),
+  type: z.enum(["income", "expense"]),
+});
+
+export const categories = new Hono()
+  .get("/", zValidator("query", getCategoriesSchema), async (c) => {
     const auth = getAuth(c);
 
     if (!auth?.userId) {
-      return c.json({ message: "You are not logged in." }, 401);
+      return c.json({ message: "ログインしてください" }, 401);
     }
 
     const { type } = c.req.valid("query");
@@ -34,5 +41,39 @@ export const categories = new Hono().get(
       );
 
     return c.json({ categories });
-  },
-);
+  })
+  .post("/", zValidator("json", createCategorySchema), async (c) => {
+    const auth = getAuth(c);
+
+    if (!auth?.userId) {
+      return c.json({ message: "ログインしてください" }, 401);
+    }
+
+    const input = c.req.valid("json");
+
+    const [existing] = await db
+      .select()
+      .from(categoryTable)
+      .where(
+        and(
+          eq(categoryTable.userId, auth.userId),
+          eq(categoryTable.name, input.name),
+        ),
+      )
+      .limit(1);
+
+    if (existing) {
+      return c.json({ message: "このカテゴリー名は既に使用されています" }, 400);
+    }
+
+    const [category] = await db
+      .insert(categoryTable)
+      .values({
+        id: createId(),
+        ...input,
+        userId: auth.userId,
+      })
+      .returning();
+
+    return c.json({ category });
+  });
